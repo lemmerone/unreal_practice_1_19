9ead088913de207d184b7c337992c9b57b853757
Vadim Lemmer <lemmeronne@gmail.com>
2024-03-10 16:38:13 +0600

    feat: наследование и перегрузка

---
+++ unreal_practice_1_19.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+class Animal
+{
+public:
+    virtual void voice()
+    {
+        std::cout << "\n";
+    }
+};
+
+class Dog : public Animal
+{
+public:
+    void voice() override
+    {
+        std::cout << "Woof!\n";
+    }
+};
+
+class Cat : public Animal
+{
+public:
+    void voice() override
+    {
+        std::cout << "Meow!\n";
+    }
+};
+
+class Cow : public Animal
+{
+public:
+    void voice() override
+    {
+        std::cout << "Moo!\n";
+    }
+};
+
+int main()
+{
+    Animal* animals[3] = { new Dog, new Cat, new Cow };
+
+    for (int i = 0; i < 3; i++)
+    {
+        animals[i]->voice();
+        delete animals[i];
+    }
+}
